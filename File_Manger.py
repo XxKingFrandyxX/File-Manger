277e74ea24c7e07cd567f3d649c2530d6eadb8b4
XxKingFrandyxX <151718434+XxKingFrandyxX@users.noreply.github.com>
2024-07-24 23:37:18 +1000

    Create File_Manger.py
    V1

---
+++ File_Manger.py
@@ -0,0 +1,30 @@
+import os 
+import sys
+
+Value = str("A")
+Value = input("Hello to Fernados file manager if you want your files managed press Z ")
+
+if Value == "Z":
+    print("You have decided to continue")
+else:  
+    sys.exit("Thank you for not pressing Z who saved me from writing a bunch of lines")
+
+
+list_folders = ["Pictures Folder", "Videos_Folder", "EXE folder" ,"TXT_Folder", "Jpg_Folder", "Png_Folder", "MP4_folder", "MP3_folder", "Wav_Folder", "Junk_Items"]
+
+for item in list_folders:  
+    newpath = rf"C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\{item}"
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+
+file_names = os.listdir("C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Junk_Items")
+
+print(file_names[0])
+
+for True_files in file_names:
+    number = 0 
+    file_names[number] = True_files
+    number + 1
+    True_files = True_files.split
+    print(True_files)
+
