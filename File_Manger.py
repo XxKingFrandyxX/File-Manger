cb354bf7dd52f87d681d41cfb545b541af4d95e3
XxKingFrandyxX <151718434+XxKingFrandyxX@users.noreply.github.com>
2024-10-04 23:19:33 +1000

    Finally Finshed

--- File_Manger.py
+++ File_Manger.py
@@ -2,6 +2,7 @@ import os
 import sys
 import shutil
 import wx
+from tqdm.auto import tqdm 
 
 
       #Template   
@@ -11,57 +12,65 @@ class MyFrame(wx.Frame):
         panel = wx.Panel(self)
         button_sizer = wx.BoxSizer(wx.VERTICAL) 
 
-        Button_play = wx.Button(panel, label = "Start",)
+        Button_play = wx.Button(panel, label = "Start")
+        button_quit = wx.Button(panel, label = "Quit Program") 
         button_sizer.Add(Button_play,  0, wx.ALL | wx.CENTER, 5)
+        button_sizer.Add(button_quit, 0, wx.ALL | wx.CENTER, 5)
+
+        button_quit.Bind(wx.EVT_BUTTON, self.quit)
+        Button_play.Bind(wx.EVT_BUTTON, self.start)
 
         panel.SetSizer(button_sizer)  
         self.Show()
 
-# Value = str("A")
-# Value = input("Hello to Fernados file manager if you want your files managed press Z ")
+    def quit (self, event):
+        sys.exit("See Ya Later!!")
 
-# if Value == "Z":
-#     print("You have decided to continue")
-# else:  
-#     sys.exit("Thank you for not pressing Z who saved me from writing a bunch of lines")
+    
+    def start (self, event):
+
+        for Progressbar in tqdm(range(10000)):
+            print(" ", end='\r')
 
 
-# list_folders = ["Pictures Folder", "Videos_Folder", "EXE folder" ,"TXT_Folder", "Jpg_Folder", "Png_Folder", "MP4_folder", "MP3_folder", "Wav_Folder", "Junk_Items"]
+        list_folders = ["Pictures Folder", "Videos_Folder", "EXE folder" ,"TXT_Folder", "Jpg_Folder", "Png_Folder", "MP4_folder", "MP3_folder", "Wav_Folder", "Junk_Items"]
 
-# for item in list_folders:  
-#     newpath = rf"C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\{item}"
-#     if not os.path.exists(newpath):
-#         os.makedirs(newpath)
+        for item in list_folders:  
+            newpath = rf"C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\{item}"
+            if not os.path.exists(newpath):
+                os.makedirs(newpath)
 
-# file_names = os.listdir("C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Junk_Items")
+        file_names = os.listdir("C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Junk_Items")
 
-# print(file_names)   
+        print(file_names)   
 
-# for placement in file_names:
-#     number = 0
-#     file_names[number] = placement
-#     True_files = str(placement)
-#     if True_files.endswith (".exe"):
-#         shutil.move ("C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Junk_Items\\" + True_files,"C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\EXE folder\\" + True_files)
-#         print("has been seccesfully transfered")
-#     if True_files.endswith (".txt"):
-#         shutil.move ("C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Junk_Items\\" + True_files,"C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\TXT_Folder\\" + True_files)
-#         print("has been seccesfully transfered")
-#     if True_files.endswith (".png"):
-#         shutil.move ("C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Junk_Items\\" + True_files,"C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Pictures Folder\\" + True_files)
-#         print("has been seccesfully transfered")
-#     if True_files.endswith (".mp4"):
-#         shutil.move ("C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Junk_Items\\" + True_files,"C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Videos_Folder\\" + True_files)
-#         print("has been seccesfully transfered")
-#     if True_files.endswith (".jpg"):
-#         shutil.move ("C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Junk_Items\\" + True_files,"C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Jpg_Folder\\" + True_files)
-#         print("has been seccesfully transfered")
-#     if True_files.endswith (".wav"):
-#         shutil.move ("C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Junk_Items\\" + True_files,"C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Wav_Folder\\" + True_files)
-#         print("has been seccesfully transfered")
-#     number += 1
-#     if placement == "":
-#         break
+        for placement in file_names:
+            number = 0
+            file_names[number] = placement
+            True_files = str(placement)
+            if True_files.endswith (".exe"):
+                shutil.move ("C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Junk_Items\\" + True_files,"C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\EXE folder\\" + True_files)
+                print("has been seccesfully transfered")
+            if True_files.endswith (".txt"):
+                shutil.move ("C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Junk_Items\\" + True_files,"C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\TXT_Folder\\" + True_files)
+                print("has been seccesfully transfered")
+            if True_files.endswith (".png"):
+                shutil.move ("C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Junk_Items\\" + True_files,"C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Pictures Folder\\" + True_files)
+                print("has been seccesfully transfered")
+            if True_files.endswith (".mp4"):
+                shutil.move ("C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Junk_Items\\" + True_files,"C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Videos_Folder\\" + True_files)
+                print("has been seccesfully transfered")
+            if True_files.endswith (".jpg"):
+                shutil.move ("C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Junk_Items\\" + True_files,"C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Jpg_Folder\\" + True_files)
+                print("has been seccesfully transfered")
+            if True_files.endswith (".wav"):
+                shutil.move ("C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Junk_Items\\" + True_files,"C:\\Users\\Frando\\Desktop\\Work\\Programming\\File_Manger\\Wav_Folder\\" + True_files)
+                print("has been seccesfully transfered")
+            number += 1
+            if placement == "":
+                break
+
+        
 
 if __name__ == '__main__':
     app = wx.App()
